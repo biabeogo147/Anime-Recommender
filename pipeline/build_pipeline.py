8ae52ba82851a0a1d03f7f8d39cac95dc3c45c7c
biabeogo147 <biabeogo147@gmail.com>
2025-08-20 21:40:26 +0700

    update setup.py

--- pipeline/build_pipeline.py
+++ pipeline/build_pipeline.py
@@ -5,8 +5,8 @@ from utils.logger import get_logger
 from utils.custom_exception import CustomException
 
 load_dotenv()
-
 logger = get_logger(__name__)
+
 
 def main():
     try:
@@ -15,18 +15,19 @@ def main():
         loader = AnimeDataLoader("data/anime_with_synopsis.csv" , "data/anime_updated.csv")
         processed_csv = loader.load_and_process()
 
-        logger.info("Data  loaded and processed...")
+        logger.info("Data loaded and processed...")
 
         vector_builder = VectorStoreBuilder(processed_csv)
         vector_builder.build_and_save_vectorstore()
 
-        logger.info("Vector store Built sucesfully....")
+        logger.info("Vector store Built successfully....")
 
-        logger.info("Pipelien built sucesfuly....")
+        logger.info("Pipeline built successfully....")
     except Exception as e:
             logger.error(f"Failed to execute pipeline {str(e)}")
             raise CustomException("Error during pipeline " , e)
-    
+
+
 if __name__=="__main__":
      main()
 
