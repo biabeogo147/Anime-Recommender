d46bd80a989b66cacbc1e3b96c7c48f2d1d17ad0
biabeogo147 <biabeogo147@gmail.com>
2025-08-24 17:10:28 +0700

    update create-secret

--- llmops/anime-recommender-deploy.sh
+++ llmops/anime-recommender-deploy.sh
@@ -8,14 +8,6 @@ eval $(minikube docker-env)
 docker build -t anime-recommender-app:latest .
 
 # ===============================
-# 2) Create Secret for Anime Recommender
-# ===============================
-kubectl create secret generic anime-recommender-secrets \
-  --from-literal=GOOGLE_API_KEY="" \
-  --from-literal=HUGGINGFACEHUB_API_TOKEN="" \
-  --dry-run=client -o yaml | kubectl apply -f -
-
-# ===============================
 # 5) Deploy LLMOps App + Services + ServiceMonitor + Ingress
 # ===============================
 kubectl apply -f llmops-k8s.yaml
